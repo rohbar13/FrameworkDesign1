7d51dcb4e3d51bcc884ff1d442bc053a839a1aca
rohbar13 <rohbar13@gmail.com>
2019-12-08 21:59:04 +0530

    1st commit

--- src/main/java/testing/TestClass.java
+++ src/main/java/testing/TestClass.java
@@ -1,19 +1,61 @@
 package testing;
 
+import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class TestClass {
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
-	@Test
-	public void testingHomePage() {
-		
-		System.setProperty("webdriver.chrome.driver", "drivers//chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.easemytrip.com/");
-		
-		
-	}
+import utilities.BaseClass;
+
+public class TestClass extends BaseClass{
+
+	ExtentReports extent = new ExtentReports();
+	ExtentHtmlReporter htmlreport = new ExtentHtmlReporter("test-output//extentreport.html");
 	
+	@Test
+	public void testingHomePage() throws IOException {
+		String testCaseID1 = "TC_HomePage_001";
+
+		extent.attachReporter(htmlreport);
+		
+		ExtentTest test1 = extent.createTest(testCaseID1);
+		
+		WebDriver driver = openBrowser();
+		
+		getURL(driver, "https://www.easemytrip.com/");
+		
+		String title = driver.getTitle();
+		
+		if(title.contains("flight")) {
+			test1.pass("Title Validated");
+		}else {
+			test1.fail("Title Validation Failed");
+		}
+		
+		String pic1 = captureScreenshot(driver, testCaseID1);
+		
+		test1.addScreenCaptureFromPath(pic1);
+		
+		String testCaseID2 = "TC_HomePage_002";
+		ExtentTest test2 = extent.createTest(testCaseID2);
+		
+		try
+		{
+			clickElement(driver.findElement(By.linkText("Flights")));
+			test2.pass("Test Case Passed");
+		}catch (Exception e) {
+			test2.fail("Test Case Failed");
+		}
+		
+		String pic2 = captureScreenshot(driver, testCaseID2);
+		test2.addScreenCaptureFromPath(pic2);
+		
+		driver.quit();
+		extent.flush();
+	}
 }
--- src/main/java/utilities/BaseClass.java
+++ src/main/java/utilities/BaseClass.java
@@ -1,5 +1,42 @@
 package utilities;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+
 public class BaseClass {
 
+	public WebDriver openBrowser() {
+		Reporter.log("Open Browser");
+		System.setProperty("webdriver.chrome.driver", "drivers//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		return driver;
+	}
+	
+	public void getURL(WebDriver driver,String URL) {
+		Reporter.log("Open URL "+URL);
+		driver.get(URL);
+	}
+	
+	public String captureScreenshot(WebDriver driver,String filename) throws IOException {
+		Reporter.log("Capture Screenshot");
+		TakesScreenshot tss = (TakesScreenshot)driver;
+		File src = tss.getScreenshotAs(OutputType.FILE);
+		File dst = new File("screenshot//"+filename+".png");
+		FileUtils.copyFile(src, dst);
+		return dst.getAbsolutePath();
+	}
+	
+	public void clickElement(WebElement element) {
+		Reporter.log("Click Element "+element.getText());
+		element.click();
+	}
+	
 }
